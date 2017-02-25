4b89094cc9a5c124cb5729451d61b1359301c6a3
mocor <mocor@DESKTOP-1QG8BCD>
2017-02-25 21:03:04 +0500

    start version

--- barley-break/Form1.Designer.cs
+++ barley-break/Form1.Designer.cs
@@ -32,11 +32,13 @@
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(465, 381);
+			this.ClientSize = new System.Drawing.Size(608, 624);
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Shown += new System.EventHandler(this.Form1_Shown);
 			this.Click += new System.EventHandler(this.Form1_Click);
 			this.ResumeLayout(false);
 
--- barley-break/Form1.cs
+++ barley-break/Form1.cs
@@ -19,14 +19,30 @@ namespace barley_break
 
 
 
-
 		private void Form1_Click(object sender, EventArgs e)
 		{
 			// <- Клик на форму
+			//Graphics g = this.CreateGraphics();
+
+			//Pen myPen = new Pen(Color.Black, 1);
+			//g.DrawLine(myPen, 10, 10, 100, 100);
+		}
+
+
+
+		private void Form1_Shown(object sender, EventArgs e)
+		{
 			Graphics g = this.CreateGraphics();
 
 			Pen myPen = new Pen(Color.Black, 1);
-			g.DrawLine(myPen, 10, 10, 100, 100);
+			g.DrawRectangle(myPen, 20, 20, 360, 360); // размер экрана 360 на 360
+			for (int i = 1; i <= 3; i++)
+			{
+				int pos = 20 + 90 * i;
+				g.DrawLine(myPen, 20, pos, 380, pos);
+				g.DrawLine(myPen, pos, 20, pos, 380);
+			}
+
 		}
 	}
 }
